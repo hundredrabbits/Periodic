39db0228f6759eb17bcb71c412bb9847b4325b6b
neauoire <aliceffekt@gmail.com>
2020-03-26 20:28:49 +0900

    *

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const apps = fs.readdirSync('../').filter((file) => { return file.toLowerCase() 
 // Load latest
 
 const latest = {}
+const res = {}
 
 for (const lib of libs) {
   latest[lib] = fs.readFileSync(`./lib/${lib}`, 'utf8')
@@ -14,22 +15,33 @@ for (const lib of libs) {
 
 for (const app of apps) {
   const folderpath = `../${app}/scripts/lib`
+  res[app] = {}
   if (!fs.existsSync(folderpath)) {
     continue
   }
-
-  console.log(`\nChecking ${app}..`)
-
   for (const lib of libs) {
     const libpath = `${folderpath}/${lib}`
     if (fs.existsSync(libpath)) {
       const libtxt = fs.readFileSync(libpath, 'utf8')
       const liblatest = latest[lib]
-      if (libtxt !== liblatest) {
-        console.warn(`- ${lib} is outdated!`)
-      }
+      res[app][lib] = libtxt === liblatest
     }
   }
 }
 
-console.log('\nDone.')
+console.log(`\nScanned ${apps.length} applications.\n`)
+
+for (const id in res) {
+  if (Object.keys(res[id]).length === 0) { continue }
+  const equals = Object.values(res[id]).reduce((acc, item) => { return acc + (item ? 1 : 0) }, 0)
+  const total = Object.keys(res[id]).length
+  if (equals === total) {
+    console.log(`${id} is OK.`)
+    continue
+  }
+  console.log(`${id} has ${total - equals}/${total} outdated libraries.`)
+  for (const lib in res[id]) {
+    if (res[id][lib]) { continue }
+    console.log(`! [${lib}] is outdated.`)
+  }
+}
